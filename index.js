6cb489ad7118bc65b523e4ff511d7f6f0a124e9d
Harrison South <hsouth95@hotmail.co.uk>
2017-02-07 00:39:35 +0000

    Created a demo site

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var express = require("express"),
-    mysql = require("mysql");
+    mysql = require("mysql"),
+    path = require("path");
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "",
     database: "rbch"
-});
+});*/
 
 var app = express();
 
-connection.connect(function(err){
+app.use(express.static(path.join(__dirname, "public")));
+
+/*connection.connect(function(err){
     if(!err){
         console.log("Connected!");
     } else {
         console.log("Error " + err);
     }
-});
+});*/
 
 app.get("/", function(req, res){
     connection.query("SELECT * FROM hospital", function(err, rows, fields){
@@ -26,6 +29,10 @@ app.get("/", function(req, res){
             console.log("Error with query");
         }
     });
+});
+
+app.get("/apple", function(req, res){
+    res.sendFile(__dirname + "/public/views/index.html");
 });
 
 app.listen(3000);
